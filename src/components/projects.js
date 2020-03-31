ec65b6e4748d1810f49e2ba57b9957185c48aab8
dika <dikaemanuel@gmail.com>
2020-03-31 22:39:15 +0100

    Mobile responsive changes

--- src/components/projects.js
+++ src/components/projects.js
@@ -16,72 +16,84 @@ class Projects extends Component {
         if(this.state.activeTab === 0) {
             return(
             <div className="projects-grid">
-                {/* Php Project 1 */}
-               <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                   <CardTitle style={{color:'#fff', height: '176px', background: `url(${Background}) center / cover`}}></CardTitle>
-                   <CardText>UNILANCERPRO. Built with Php OOP</CardText>
-                   <CardActions border>
-                        <a href="https://github.com/dikascode/uni-cer" rel="noopener noreferrer" target="_blank">
-                            <Button colored>Github</Button>
-                        </a>
-                        
-                        <a href="https://www.unilancerpro.com" rel="noopener noreferrer" target="_blank"><Button colored>Website</Button></a>
-                   </CardActions>
-                   <CardMenu style={{color:'#fff'}}>
-                       <IconButton name="share" />
-                   </CardMenu>
-               </Card>
-
-
-                {/* Php Project 2 */}
-                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color:'#fff', height: '176px', background: `url(${Background2}) center / cover`}}></CardTitle>
-                    <CardText>YouConnect. Event Ticketing Web application built with Php OOP/MVC</CardText>
-                    <CardActions border>
-                        <a href="https://github.com/dikascode/youconnect" rel="noopener noreferrer" target="_blank">
-                            <Button colored>Github</Button>
-                        </a>
-                        
-                        <a href="https://youconnect.herokuapp.com" rel="noopener noreferrer" target="_blank"><Button colored>Website</Button></a>
-                    </CardActions>
-                    <CardMenu style={{color:'#fff'}}>
-                        <IconButton name="share" />
-                    </CardMenu>
-                </Card>
-
-                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                   <CardTitle style={{color:'#fff', height: '176px', background: `url(${Background3}) center / cover`}}></CardTitle>
-                   <CardText>OurMarket eCommerce App</CardText>
-                   <CardActions border>
-                        <a href="https://github.com/dikascode/ourMarket" rel="noopener noreferrer" target="_blank">
-                            <Button colored>Github</Button>
-                        </a>
-                        
-                        <a href="https://ourmarket-demo-app.herokuapp.com/public/index.php" rel="noopener noreferrer" target="_blank"><Button colored>Website</Button></a>
-                   </CardActions>
-                   <CardMenu style={{color:'#fff'}}>
-                       <IconButton name="share" />
-                   </CardMenu>
-               </Card>
-            </div>
-            )
-        } else if(this.state.activeTab === 1) {
-            return(
-                <div className="project-grid">
-                     <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                        <CardTitle style={{color:'#fff', height: '176px', background: `url(${Background4}) center / cover`}}></CardTitle>
-                        <CardText>My Portfolio</CardText>
+            <Grid>
+                <Cell col={4}>
+                        {/* Php Project 1 */}
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color:'#fff', height: '176px', background: `url(${Background}) center / cover`}}></CardTitle>
+                        <CardText>UNILANCERPRO. Built with Php OOP</CardText>
                         <CardActions border>
-                                <a href="https://github.com/dikascode/DUE_portfolio" rel="noopener noreferrer" target="_blank">
+                                <a href="https://github.com/dikascode/uni-cer" rel="noopener noreferrer" target="_blank">
                                     <Button colored>Github</Button>
                                 </a>
                                 
-                                <a href="#" rel="noopener noreferrer" target="_blank"><Button colored>Website</Button></a>
+                                <a href="https://www.unilancerpro.com" rel="noopener noreferrer" target="_blank"><Button colored>Website</Button></a>
                         </CardActions>
                         <CardMenu style={{color:'#fff'}}>
                             <IconButton name="share" />
                         </CardMenu>
                     </Card>
+                </Cell>
+
+                <Cell col={4}>
+                        {/* Php Project 2 */}
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                            <CardTitle style={{color:'#fff', height: '176px', background: `url(${Background2}) center / cover`}}></CardTitle>
+                            <CardText>YouConnect app built with Php OOP/MVC</CardText>
+                            <CardActions border>
+                                <a href="https://github.com/dikascode/youconnect" rel="noopener noreferrer" target="_blank">
+                                    <Button colored>Github</Button>
+                                </a>
+                                
+                                <a href="https://youconnect.herokuapp.com" rel="noopener noreferrer" target="_blank"><Button colored>Website</Button></a>
+                            </CardActions>
+                            <CardMenu style={{color:'#fff'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                </Cell>
+
+                <Cell col={4}>
+
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color:'#fff', height: '176px', background: `url(${Background3}) center / cover`}}></CardTitle>
+                        <CardText>OurMarket eCommerce App</CardText>
+                        <CardActions border>
+                                <a href="https://github.com/dikascode/ourMarket" rel="noopener noreferrer" target="_blank">
+                                    <Button colored>Github</Button>
+                                </a>
+                                
+                                <a href="https://ourmarket-demo-app.herokuapp.com/public/index.php" rel="noopener noreferrer" target="_blank"><Button colored>Website</Button></a>
+                        </CardActions>
+                        <CardMenu style={{color:'#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </Cell>
+               </Grid>
+            </div>
+            )
+        } else if(this.state.activeTab === 1) {
+            return(
+                <div className="project-grid">
+                    <Grid>
+                        <Cell col={4}>
+                            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                                <CardTitle style={{color:'#fff', height: '176px', background: `url(${Background4}) center / cover`}}></CardTitle>
+                                <CardText>My Portfolio</CardText>
+                                <CardActions border>
+                                        <a href="https://github.com/dikascode/DUE_portfolio" rel="noopener noreferrer" target="_blank">
+                                            <Button colored>Github</Button>
+                                        </a>
+                                        
+                                        <a href="#" rel="noopener noreferrer" target="_blank"><Button colored>Website</Button></a>
+                                </CardActions>
+                                <CardMenu style={{color:'#fff'}}>
+                                    <IconButton name="share" />
+                                </CardMenu>
+                            </Card>
+                        </Cell>
+                    </Grid>
                 </div>
             )
         } else if (this.state.activeTab === 2) {
