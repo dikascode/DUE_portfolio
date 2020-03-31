31c445c01cc6126cb55ab558798b89421e622922
dika <dikaemanuel@gmail.com>
2020-03-31 20:01:43 +0100

    Build

---
+++ build/precache-manifest.48cd2e5bcc366587d4d6d0bfcee0860f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8343919d7d900eae2e89489f47e9ce8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0820bb52fb406f8c62e",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "b10da89aec4df861ba1a",
+    "url": "/static/css/main.f321a5d7.chunk.css"
+  },
+  {
+    "revision": "a0820bb52fb406f8c62e",
+    "url": "/static/js/2.fed8f2d2.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.fed8f2d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10da89aec4df861ba1a",
+    "url": "/static/js/main.41031c2d.chunk.js"
+  },
+  {
+    "revision": "eb81441c62b43b4493e5",
+    "url": "/static/js/runtime-main.543e3e97.js"
+  },
+  {
+    "revision": "7c70d917a8423d1d758999811cfa8585",
+    "url": "/static/media/Dika_2.7c70d917.png"
+  },
+  {
+    "revision": "d8f877d178b837ece695b4988179fa02",
+    "url": "/static/media/ourmarket1.d8f877d1.jpg"
+  },
+  {
+    "revision": "2b90090807a7ee2ce23a29160561fa7f",
+    "url": "/static/media/portfolio.2b900908.jpg"
+  },
+  {
+    "revision": "ba449c7fbf5449c0ca2aec9f019d2d6b",
+    "url": "/static/media/unilance.ba449c7f.jpg"
+  },
+  {
+    "revision": "e29d78db72b7714fa5438b129021d039",
+    "url": "/static/media/youconnect.e29d78db.jpg"
+  }
+]);
